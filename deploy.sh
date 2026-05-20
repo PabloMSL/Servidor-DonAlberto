eadcbf54d2fc1d6c5f50a6e2d6cd7aac84fa266b
PabloMSL <jpablomilitar@gmail.com>
2026-05-20 08:49:23 +0000

    deploy

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Iniciando el despliegue automatico"
+
+cd /home/juanpablomozuca/moto-api
+
+echo "Trayendo la ultima version desde git"
+
+git pull origin master
+
+echo "Asegurando las dependencias"
+source venv/bin/activate
+pip install -r requirements.txt --quiet
+
+
+echo "Reiniciando gunicorn"
+sudo systemctl restart moto-api.service
+
+
+echo "Despliegue completado. Estado Actual:"
+sudo systemctl status moto-api.service | grep "Activate:" 
