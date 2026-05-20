5d7d86529c2c97047761c87badf43f0d61a968d7
PabloMSL <jpablomilitar@gmail.com>
2026-05-20 07:35:15 +0000

    primer commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import  Flask, jsonify, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/api/repuestos')
+def get_repuestos():
+        return jsonify({
+          "status" : "Online",
+          "servidor" : "Linux-Ubuntu usado por pablo",
+          "hora_servidor" : str(datetime.datetime.now()),
+          "inventario" : ["Bujias de Iridio", "Filtro de aceite", "Aceite motul 7100"]
+        })
+
+peritajes_db = [{"placa":"ABC-123", "estado": "En revision"}]
+
+@app.route('/api/registros', methods=['GET'])
+def get_registros():
+    return jsonify({
+       "servidor": "Servidor Mozuca",
+       "hora_servidor": str(datetime.datetime.now()),
+       "inventario": ["Llanta delantera", "Aceite 10W40", "Cadena Reforzada"]
+    })
+
+@app.route('/api/peritajes', methods=['GET', 'POST'])
+def gestionar_peritajes():
+    if request.method == 'POST':
+        datos = request.get_json()
+        nueva_moto = {"placa": datos['placa'], "estado": "Recibida"}
+        peritajes_db.append(nueva_moto)
+        return jsonify({"mensaje": "Registro Exitoso", "moto": nueva_moto}), 201
+    return jsonify(peritajes_db)
+
+
+if __name__ == "__main__":
+         app.run()
