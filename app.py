da158c2770974c0de3a1b7e7085b71f02f402518
PabloMSL <jpablomilitar@gmail.com>
2026-06-16 11:37:46 -0500

    Arreglo en app.py

--- app.py
+++ app.py
@@ -36,7 +36,4 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
 
-@app.route('/api/inventario', methods=['GET', 'POST'])
-def gestionar_inventario():
-      if request.method == 'POST':
 
